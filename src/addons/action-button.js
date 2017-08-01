32458ff5305f617f8b0edb11948468955432a151
Solomon Hawk <solomon.hawk@viget.com>
2017-08-01 12:53:38 -0400

    #381 Add prepare hook for action-button

--- src/addons/action-button.js
+++ src/addons/action-button.js
@@ -24,11 +24,13 @@ class ActionButton extends React.PureComponent {
   }
 
   static defaultProps = {
-    tag: 'button'
+    tag: 'button',
+    prepare: (event, value) => value
   }
 
   click(event: Event): Action {
-    let action = this.send(this.props.action, this.props.value)
+    let payload = this.props.prepare(event, this.props.value)
+    let action = this.send(this.props.action, payload)
 
     if (action && action instanceof Action) {
       action
@@ -58,6 +60,7 @@ class ActionButton extends React.PureComponent {
     delete props.onCancel
     delete props.onError
     delete props.send
+    delete props.prepare
 
     if (this.props.tag === 'button' && props.type == null) {
       props.type = 'button'
